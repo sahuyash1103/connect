2753d9c637463ff68e65ab63710191cdd0015bac
yash sahu <sahuyash1103@gmail.com>
2024-11-05 09:38:58 +0530

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,17 @@ export default [
     files: ['**/*.{js,mjs,cjs,ts}'], // Specify the file patterns directly here
     languageOptions: { sourceType: 'commonjs' },
   },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      // Set these specific rules to warning level
+      'no-unused-vars': 'warn', // Warn for unused variables
+      'no-unused-imports': 'warn', // Warn for unused imports
+      '@typescript-eslint/no-explicit-any': 'warn', // Warn for usage of `any` type
+    },
+  },
 ];
